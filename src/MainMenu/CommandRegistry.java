5c076e056830534655e4ecd652c8009fd341a174
keyshiiie <130091750+keyshiiie@users.noreply.github.com>
2026-07-27 00:13:26 +0300

    removed wildcard import
    Виктор говорил, что звездочка в импорте - это дурной тон и нужны явные импорты

--- src/MainMenu/CommandRegistry.java
+++ src/MainMenu/CommandRegistry.java
@@ -1,6 +1,9 @@
 package MainMenu;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class CommandRegistry {
     private final Map<String, ConsoleCommand> commands = new HashMap<>();
